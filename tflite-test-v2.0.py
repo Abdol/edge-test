552d1689ca512e9000ef578afa561870dfce7318
abdol <a@fkrtech.com>
2021-12-19 06:31:50 +0000

    update files and code

--- tflite-test-v2.0.py
+++ tflite-test-v2.0.py
@@ -1,16 +1,20 @@
-print('Hi')
+print('TFLite LSTM Energy Classivation V2.0')
 
 # imports
 import tflite_runtime.interpreter as tflite
 import numpy as np
 import pandas as pd
 import cv2
+import time
+from datetime import datetime
+import csv
+import multiprocessing
 
 # definitions
-foldername = 'gaf_output_all'
-filename = 'dataset_edge_test-v2.0.csv'
+foldername = 'gaf_output_eval'
+filename = 'output_dataset_eval.csv'
 files_list = []
-labels = ['normal', 'abnormal']
+labels = ['abnormal', 'normal']
 predictions = []
 
 # import data
@@ -19,6 +23,7 @@ files_list = edge_dataset['image_name'].values
 print(edge_dataset.head())
 
 # prepare tflite model
+tflite_model_eval2_time_start = time.clock()
 tflite_model_path = 'model.tflite'
 interpreter = tflite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
@@ -34,7 +39,7 @@ print('width:', width, 'height', height)
 
 # execute model (i.e. infer) on data files
 for file in files_list:
-  image_path = foldername + '/' + file # 'dandelion.jpg' # 'rose.jpg' # 'sunflower.jpg' # 'daisy.jpg' # 'daisy.jpg'
+  image_path = foldername + '/' + file
   image = cv2.imread(image_path)
   image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
   imH, imW, _ = image.shape 
@@ -44,18 +49,33 @@ for file in files_list:
   # Perform the actual detection by running the model with the image as input
   interpreter.set_tensor(input_details[0]['index'],input_data)
   interpreter.invoke()
-
   prediction_tensor = interpreter.get_tensor(output_details[0]['index']) 
   prediction = np.argmax(prediction_tensor) + 1
   prediction_label = labels[np.argmax(prediction_tensor)]
   _dataset_prediction = edge_dataset.loc[edge_dataset['image_name'] == file].values[0][2]
-  # print(file, _dataset_prediction)
   prediction_outcome = (prediction != _dataset_prediction)
   predictions.append(prediction_outcome)
   print('Filename:', file, '- TFLite tensor:', prediction_tensor, '- prediction label:', prediction_label, '- prediction:', prediction, '- isTrue?', prediction_outcome)
+tflite_model_eval2_time_elapsed = time.clock() - tflite_model_eval2_time_start
 
-# calculate model evaluation metrics
+# Calculate model evaluation metrics
 predictions = np.array(predictions)
 no_true_positives = len(predictions[predictions == 1])
 tflite_accuracy = no_true_positives / len(predictions) # (no. of correct prediction)/(no. of all predictions)
 print('TFLite accuracy: ', tflite_accuracy)
+
+# Print running stats
+timestamp = str(datetime.now())
+stats = f'''Run stats (in sec) - {timestamp}
+---------------------------TFLite-----------------------
+tflite_model_eval2_time_elapsed (using interpreter): {tflite_model_eval2_time_elapsed} - accuracy: {tflite_accuracy}
+-------------------------------------------------------
+No. of CPU cores: {multiprocessing.cpu_count()}
+'''
+print(stats)
+
+# csv file header: ['timestamp', 'tflite_eval_time_elapsed', 'accuracy'])
+stats_list = [timestamp, tflite_model_eval2_time_elapsed, tflite_accuracy['accuracy']]
+with open('stats_odroid' + '.csv', "a") as csv_output_file:
+    writer = csv.writer(csv_output_file)
+    writer.writerow(stats_list)
